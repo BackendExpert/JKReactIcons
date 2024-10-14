8fdf7e7cce54111b57eada378fb5948dad6cae01
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-14 15:02:47 +0530

    Update

---
+++ src/components/Dashboard/Content.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import SideBar from './SideBar'
+import NavBar from './NavBar'
+import Footer from './Footer'
+
+
+const Content = () => {
+  return (
+    <div className="w-full min-h-screen bg-blue-100/50 ">
+        <div className="md:flex">
+        <div className="w-1/5 fixed top-0 left-0 h-full">
+            <SideBar />
+        </div>
+        <div className="md:w-full md:ml-[270px]"> 
+            <NavBar />
+            <div className="py-4 px-2 md:mr-4  md:mx-0 ml-4 mr-4">
+                <Outlet />
+            </div>
+            <Footer />
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Content
