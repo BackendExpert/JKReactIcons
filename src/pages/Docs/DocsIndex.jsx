5f3fe1c679739dec3c4956853ac5d673370ba1e2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-15 20:01:56 +0530

    Update

--- src/pages/Docs/DocsIndex.jsx
+++ src/pages/Docs/DocsIndex.jsx
@@ -9,8 +9,20 @@ const DocsIndex = () => {
 
         <p className='my-4'>Docs and all Icons of this Framework</p>
 
+        <p className='my-4'>These Icons Based on Bootstrap Icons </p>
+
+        <p className='my-4'>This Icon Framework Developed using <a href="https://icons.getbootstrap.com/" target="_blank" className='text-purple-500 font-semibold'>Bootstrap Icons</a></p>
+
         <div className="md:grid grid-cols-4 gap-8">
-            
+            {Object.keys(Icons).map((iconName) => {
+                const IconComponent = Icons[iconName];
+                return (
+                <div key={iconName} className="flex flex-col items-center text-center my-4">
+                    <IconComponent size={44} />
+                    <span className="mt-2 text-sm">{iconName}</span>
+                </div>
+                );
+            })}
         </div>
     </div>
   )
