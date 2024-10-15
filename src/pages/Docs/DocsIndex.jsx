76f18a1a591bb0d779a7292a537de1f8121c16a7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-15 19:58:21 +0530

    Update

--- src/pages/Docs/DocsIndex.jsx
+++ src/pages/Docs/DocsIndex.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import * as Icons from '@jehankandy/jkreact-icons';
 
 const DocsIndex = () => {
   return (
-    <div>DocsIndex</div>
+    <div className="mt-8">
+        <h1 className="font-semibold text-purple-500 text-2xl">Docs</h1>
+        <hr />
+
+        <p className='my-4'>Docs and all Icons of this Framework</p>
+
+        <div className="md:grid grid-cols-4 gap-8">
+            
+        </div>
+    </div>
   )
 }
 
---
+++ src/pages/Docs/DocsOne.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DocsOne = () => {
+  return (
+    <div>DocsOne</div>
+  )
+}
+
+export default DocsOne
